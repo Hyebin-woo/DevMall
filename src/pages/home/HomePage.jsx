a7d421980bcc7bdb092e70ebde5aa43d450f8070
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-04 02:42:07 +0900

    :lipstick: Style: 홈페이지 마크업 구현

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+
 import Header from "../../components/header/Header";
+import Banner from "../../components/banner/Banner";
+import { ProdWrap } from "./style";
+import ProdCard from "../../components/prod-card/ProdCard";
+import Footer from "../../components/footer/Footer";
 
 const HomePage = () => {
     return (
         <>
             <Header />
+            <Banner />
+            <ProdWrap>
+                <ProdCard />
+            </ProdWrap>
+            <Footer />
         </>
     );
 };
---
+++ src/pages/home/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ProdWrap = styled.section`
+    width: 1280px;
+    margin: 80px auto;
+`;
