5093999eea4b683705d6ab401815c7dce8f64d3c
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-03 19:22:55 +0900

    :truck: Add: 라우터 페이지 구현

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home/HomePage";
+import LoginPage from "../pages/login/LoginPage";
+import JoinPage from "../pages/join/JoinPage";
+import ProdDetailPage from "../pages/prod-detail/ProdDetailPage";
+import CartPage from "../pages/cart/CartPage";
+import PaymentPage from "../pages/payment/PaymentPage";
+import NotFoundPage from "../pages/404/NotFoundPage";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/join" element={<JoinPage />} />
+                <Route path="/detail" element={<ProdDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/payment" element={<PaymentPage />} />
+                <Route path="/*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
