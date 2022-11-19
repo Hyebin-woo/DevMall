97a0721f5e1e82d1ad2cac310a66066b7bb031db
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-19 23:42:11 +0900

    :lipstick: Style: 판매자 등록페이지 추가

---
+++ src/pages/seller-makeprod/AddProdPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import AddProd from "../../components/prod-add/AddProd";
+import { Main, Wrap, Aside, SideBox } from "./style";
+import Header from "../../components/header/Header";
+
+const AddProdPage = () => {
+    return (
+        <>
+            <Header />
+            <Main>
+                <h1>상품등록</h1>
+                <Wrap>
+                    <Aside>
+                        <h3>* 상품 등록 주의사항</h3>
+                        <SideBox>
+                            - 너무 귀여운 사진은 심장이 아파올 수 있습니다.{" "}
+                            <br />
+                            <br />- 재고와 가격을 정확하게 입력했는지
+                            확인부탁드립니다.
+                            <br />
+                            <br /> - 상품 사진 화질에 신경써주세요.
+                            <br />
+                            <br /> - 제품에 이상이 없는지 한번 더 검토
+                            부탁드립니다.
+                        </SideBox>
+                    </Aside>
+                    <AddProd />
+                </Wrap>
+            </Main>
+        </>
+    );
+};
+
+export default AddProdPage;
---
+++ src/pages/seller-makeprod/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+export const Main = styled.main`
+    margin: 0 auto;
+    padding: 44px 150px 136px;
+    h1 {
+        font-weight: 700;
+        font-size: 36px;
+        margin-bottom: 42px;
+    }
+`;
+export const Wrap = styled.div`
+    display: flex;
+`;
+export const Aside = styled.aside`
+    width: 250px;
+    h3 {
+        font-weight: 600;
+        font-size: 16px;
+        color: #eb5757;
+    }
+`;
+
+export const SideBox = styled.div`
+    width: 250px;
+    height: 346px;
+    padding: 20px;
+    background: #ffefe8;
+    border-radius: 5px;
+    margin-top: 15px;
+    line-height: 20px;
+`;
