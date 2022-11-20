0fdde9801c8668a4cb77b73bac70797c24211b3d
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-20 21:47:14 +0900

    :lipstick: Style: 결제 페이지 구현

--- src/pages/payment/PaymentPage.jsx
+++ src/pages/payment/PaymentPage.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Main, Title, InfoSection } from "./style";
+import ThBox from "../../components/atom/thbox/ThBox";
+
+import Header from "../../components/header/Header";
+import PaymentCard from "../../components/payment/payment-card/PaymentCard";
+import PaymentDelivery from "../../components/payment/payment-delivery/PaymentDelivery";
+import PaymentMethod from "../../components/payment/payment-method/PaymentMethod";
+import PaymentAgree from "../../components/payment/payment-agree/PaymentAgree";
 
 const PaymentPage = () => {
-    return <div>PaymentPage</div>;
+    return (
+        <>
+            <Header />
+            <Main>
+                <Title>주문/결제하기</Title>
+                <InfoSection>
+                    <ThBox
+                        bgColor={"#F2F2F2"}
+                        one={"상품정보"}
+                        two={"할인"}
+                        three={"배송비"}
+                        four={"주문금액"}
+                    />
+                    <ul>
+                        <PaymentCard />
+                        <PaymentCard />
+                    </ul>
+                </InfoSection>
+
+                <PaymentDelivery />
+                <PaymentMethod />
+                <PaymentAgree />
+            </Main>
+        </>
+    );
 };
 
 export default PaymentPage;
---
+++ src/pages/payment/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    width: 1100px;
+    margin: 0 auto;
+    padding: 44px 0 96px;
+`;
+
+export const Title = styled.h2`
+    font-weight: 700;
+    font-size: 36px;
+    text-align: center;
+`;
+export const InfoSection = styled.section`
+    height: 412px;
+    margin-top: 52px;
+`;
