02b5e15e5ad7437287e6939a970dc0a9de3f21eb
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-21 20:14:39 +0900

    :lipstick: Style: 404페이지 마크업 구현

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Wrap, Txt, BtnBack } from "./style";
+import NotFound from "../../assets/images/404.svg";
 
 const NotFoundPage = () => {
-    return <div>NotFoundPage</div>;
+    const navigate = useNavigate();
+
+    return (
+        <Wrap>
+            <img src={NotFound} alt="404이미지" />
+            <Txt>페이지를 찾을 수 없습니다.</Txt>
+            <BtnBack type="button" onClick={() => navigate(-1)}>
+                이전 페이지
+            </BtnBack>
+        </Wrap>
+    );
 };
 
 export default NotFoundPage;
---
+++ src/pages/404/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Wrap = styled.section`
+    display: flex;
+    margin: 200px auto;
+    place-items: center;
+
+    flex-direction: column;
+    width: 399px;
+    height: 44px;
+    img {
+        width: 276px;
+        height: 236px;
+        text-align: center;
+    }
+`;
+export const Txt = styled.h1`
+    font-weight: 700;
+    font-size: 36px;
+    margin-top: 46px;
+`;
+
+export const BtnBack = styled.button`
+    width: 100%;
+    margin-top: 46px;
+    padding: 19px;
+    color: #ffffff;
+    background: #3a4768;
+    border-radius: 10px;
+    font-weight: 700;
+    font-size: 18px;
+`;
